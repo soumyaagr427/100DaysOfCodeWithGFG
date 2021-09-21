5c488b91a2533719777e0839a37fc26b4b9ec044
Soumya Agrawal <soumya.agr6@gmail.com>
2021-09-21 19:06:20 +0530

    Day-7th
    max area between array elements

---
+++ dam of candies.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{
+    
+    public:
+    int maxCandy(int height[], int n) 
+    { 
+        // Your code goes here
+        int curr_area=0;
+        int max_area=0;
+        int i=0;
+        int j=n-1;
+        
+        while(i<j)
+        {
+            
+            curr_area=(min(height[j],height[i]))*(j-i-1);
+            
+            
+            if(curr_area>=max_area)
+                max_area=curr_area;
+                
+            if(height[i]<=height[j])
+                i++;
+            else
+                j--;
+                
+        }
+        
+        return max_area;
+    }   
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	int height[n];
+    	for (int i = 0; i < n; ++i)
+    	{
+    		cin>>height[i];
+    	}
+    	
+    	Solution obj;
+    	
+    	cout << obj.maxCandy(height,n)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends

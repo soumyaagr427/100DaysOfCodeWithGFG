172ef87c83b8f67803dc6d2dc2319b1ab6fa0ba3
Soumya Agrawal <soumya.agr6@gmail.com>
2022-02-11 20:10:24 +0530

    used pair from stl and greedy approach

---
+++ Activity_selction.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        //Have to sort both arrays correspondin to each other. so use pair
+        
+        vector<pair<int ,int >> p;
+        
+        for(int i=0;i<n;i++)
+            p.push_back(make_pair(end[i],start[i]));
+            
+        sort(p.begin(),p.end());
+        
+        
+        int x=p[0].first;  // first cause we added end array first in the pair
+        int c=1;
+        
+        for(int i=1;i<n;i++)
+        {
+            if(p[i].second>x) // second is for the start array
+            {
+                c++;
+                x=p[i].first;
+            }
+        }
+        
+        return c;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    while(t--)
+    {
+        //size of array
+        int n;
+        cin >> n;
+        vector<int> start(n), end(n);
+        
+        //adding elements to arrays start and end
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        Solution ob;
+        //function call
+        cout << ob.activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+  
